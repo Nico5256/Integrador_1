db9185ef31aba6901b719ab9073cce5e2bb76d77
l u c i a n o <lucianocamano8@gmail.com>
2024-10-14 22:01:40 -0300

    carrusel terminado

---
+++ techcore.js
@@ -0,0 +1,25 @@
+let indiceImagen = 0;
+mostrarImagen(indiceImagen);
+
+function cambiarImagen(n){
+mostrarImagen(indiceImagen += n );
+}
+
+function mostrarImagen(n){
+    let imagenes = document.querySelectorAll(".carrousel-imagenes img");
+
+    if (n >= imagenes.length){
+        indiceImagen = 0;
+    }
+    if (n < 0){
+indiceImagen = imagenes.length - 1;
+    }
+
+imagenes.forEach(img => img.style.display = "none" );
+
+imagenes[indiceImagen].style.display = "block";
+
+}
+
+
+
