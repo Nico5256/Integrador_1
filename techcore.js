4d835f6e02832133bb2f71aec091d12cbf847829
l u c i a n o <lucianocamano8@gmail.com>
2024-10-17 00:48:29 -0300

    pagina de inicio con nuevo carrousel y letras animadas

--- techcore.js
+++ techcore.js
@@ -1,29 +1,54 @@
-let indiceImagen = 0;
-mostrarImagen(indiceImagen);
+//carrousel
+const imagenes = ["imagenes/pc1.png", "imagenes/pc2.png", "imagenes/pc3.png"];
 
-function cambiarImagen(n){
-mostrarImagen(indiceImagen += n );
+const imagenCarrousel = document.getElementById("carrousel-imagenes");
+
+let indiceActual = 0;
+
+function mostrarImagen(indice) {
+  imagenCarrousel.src = imagenes[indice];
 }
 
-function mostrarImagen(n){
-    let imagenes = document.querySelectorAll(".carrousel-imagenes img");
+mostrarImagen(indiceActual);
 
-    if (n >= imagenes.length){
-        indiceImagen = 0;
-    }
-    if (n < 0){
-indiceImagen = imagenes.length - 1;
-    }
-
-imagenes.forEach(img => img.style.display = "none" );
-
-imagenes[indiceImagen].style.display = "block";
-
+function avanzarImagen() {
+  indiceActual++;
+  if (indiceActual >= imagenes.length) {
+    indiceActual = 0;
+  }
+  mostrarImagen(indiceActual);
 }
 
+function retrocederImagen() {
+  indiceActual--;
+  if (indiceActual < 0) {
+    indiceActual = imagenes.length - 1;
+  }
+  mostrarImagen(indiceActual);
+}
 
+document.getElementById("sig-button").addEventListener("click", avanzarImagen);
+document
+  .getElementById("ant-button")
+  .addEventListener("click", retrocederImagen);
 
+//texto animado
 
+const textoCompleto =
+  "TechCore: Tu Tienda de PCs Armadas . En TechCore, nos especializamos en la venta de PCs ya armadas, diseñadas para ofrecer un rendimiento excepcional y una experiencia de usuario inigualable. Ya seas un gamer, un profesional o un usuario que busca una computadora fiable, tenemos la opción perfecta para ti";
 
+const elementoTexto = document.getElementById("texto");
 
+let valor = 0;
 
+function animarTexto() {
+  elementoTexto.textContent = textoCompleto.slice(0, valor);
+  valor++;
+
+  if (valor > textoCompleto.length) {
+    valor = 0;
+  }
+  setTimeout(animarTexto, 100);
+}
+
+animarTexto();
